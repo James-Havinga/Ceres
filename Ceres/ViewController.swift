d250968857fa94796a1a4bf895947123146d2a19
James-Havinga <180099@virtualwindow.co.za>
2020-06-01 02:44:27 +0200

    added page 4 + custom trigger + custom behaviour

--- Ceres/ViewController.swift
+++ Ceres/ViewController.swift
@@ -12,6 +12,9 @@ import RealityKit
 class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
+    @IBOutlet weak var textPanel: UIVisualEffectView!
+    @IBOutlet weak var dropButton: UIButton!
+    @IBOutlet weak var websiteLink: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,5 +24,46 @@ class ViewController: UIViewController {
         
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
+        
+        //Custom Behaviour
+        
+        boxAnchor.actions.showInterface.onAction = handleShowInterface(_:)
+        
+        boxAnchor.actions.startDropGrapes.onAction = handleDropGrapes(_:)
+    }
+    
+    //MARK: - Custom Behaviour
+    
+    func handleShowInterface(_ entity: Entity?) {
+        guard entity != nil else {
+            return
+        }
+        
+        print("Scene Started")
+        
+        textPanel.isHidden = false
+        websiteLink.isHidden = false
+        dropButton.isHidden = true
+    }
+    
+    func handleDropGrapes(_ entity: Entity?) {
+        guard  entity != nil else {
+            return
+        }
+        
+        print("Drop Grapes")
+        
+        textPanel.isHidden = false
+        websiteLink.isHidden = true
+        dropButton.isHidden = false
+    }
+    
+    //MARK: - Custom Trigger
+    
+    @IBAction func onDropGrapesTapped(_ sender: Any) {
+        if let sceneAnchor = arView.scene.anchors[0] as? Experience.Box {
+            
+            sceneAnchor.notifications.dropGrapes.post()
+        }
     }
 }
